814adf71b93b22f5ef579e59a97e9246f31204c8
marks214 <aimee.oz.x@gmail.com>
2021-01-06 11:06:19 -0800

    PlayerSubmissionForm renders a set of hard-coded inputs (adjective, noun, adverb, verb, adjective, noun). Player is able to fill out the six text input fields (can press submit and fields will clear). Placeholder text in each text input describes what to fill in.

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -3,7 +3,29 @@ import PropTypes from 'prop-types';
 
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
+  const initState = {
+    adj1: '',
+    noun1: '',
+    adverb: '',
+    verb: '',
+    adj2: '',
+    noun2: ''
+  };
+
+  const [poemLine, setPoemLine] = useState(initState);
+
+  const onPoemInputChange = (event) => {
+    const newLine = {
+      ...poemLine,
+    };
+
+    newLine[event.target.name] = [event.target.value]
+    setPoemLine(newLine);
+  }
+
+
+
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
@@ -12,12 +34,47 @@ const PlayerSubmissionForm = () => {
 
         <div className="PlayerSubmissionForm__poem-inputs">
 
-          {
-            // Put your form inputs here... We've put in one below as an example
-          }
+        <input
+            name='adj1'
+            placeholder="adjective"
+            value={poemLine.adjective1}
+            type="text"
+            onChange={onPoemInputChange} />
+            
           <input
-            placeholder="hm..."
-            type="text" />
+            name='noun1'
+            placeholder="noun"
+            value={poemLine.noun1}
+            type="text"
+            onChange={onPoemInputChange} />
+
+          <input
+            name='adverb'
+            placeholder="adverb"
+            value={poemLine.adverb}
+            type="text"
+            onChange={onPoemInputChange} />
+
+          <input
+            name='verb'
+            placeholder="verb"
+            value={poemLine.verb}
+            type="text"
+            onChange={onPoemInputChange} />
+
+          <input
+            name='adj2'
+            placeholder="adjective"
+            value={poemLine.adjective2}
+            type="text"
+            onChange={onPoemInputChange} />
+            
+          <input
+            name='noun2'
+            placeholder="noun"
+            value={poemLine.noun2}
+            type="text"
+            onChange={onPoemInputChange} />
 
         </div>
 
